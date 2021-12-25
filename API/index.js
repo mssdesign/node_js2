db26bc8c40b2797c00776de6053c90e1ecf67246
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-12-25 15:59:36 -0300

    50p

--- API/index.js
+++ API/index.js
@@ -1,3 +1,4 @@
+// AULA 232 CURSO DE NODE MAESTRIA
 const express = require('express')
 const app = express()
 
@@ -16,6 +17,14 @@ app.get('/', (req, res) => {
     //Criando resposta da api:
     res.json({message: 'Primeira rota criada'})
 
+})
+
+app.post('/createproduct', (req, res) => {
+    const name = req.body.name
+    const price = req.body.price
+
+    console.log(name, price)    //Ou salvar no banco
+    res.json({message: 'O produto foi criado!'}) //resposta da requisição
 })
 
 app.listen(3000)
